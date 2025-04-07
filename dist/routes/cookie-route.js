06e95e61106b0f121788ec2c6ed1e47094eee06b
radz22 <radzsantillan01@gmail.com>
2025-04-08 00:40:10 +0800

    Stop tracking dist directory

--- dist/routes/cookie-route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const cookie_controller_1 = require("../controllers/cookie-controller");
-const cookieRoute = (0, express_1.Router)();
-cookieRoute.get("/", cookie_controller_1.getCookies);
-exports.default = cookieRoute;
