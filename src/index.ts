905cdcbb310bc3abcbb2488437b35b7a9ffda5db
radz22 <radzsantillan01@gmail.com>
2025-04-14 21:56:55 +0800

    chore: change

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb-connection";
 import { errorHandler } from "./middleware/error-handler";
-import bodyParser from "body-parser";
 
 import authRouter from "./routes/auth-route";
 import cencusRouter from "./routes/cencus-route";
@@ -32,7 +31,7 @@ app.use(
 );
 app.use(express.json({ limit: "100mb" }));
 app.use(cookieParser());
-app.use(express.urlencoded({ limit: "200mb", extended: true }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/cencus", cencusRouter);
